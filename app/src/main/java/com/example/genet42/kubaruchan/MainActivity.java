c879396a16ddc6b5ba6ee1800d951e7810e4cbcd
Bovini <bovini@baldr.jp>
2016-01-14 16:22:58 +0900

    added ideas for UI

--- app/src/main/java/com/example/genet42/kubaruchan/MainActivity.java
+++ app/src/main/java/com/example/genet42/kubaruchan/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.genet42.kubaruchan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.example.genet42.kubaruchan.ui.TestAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridRoot = (GridView) findViewById(R.id.gridRoot);
+        gridRoot.setAdapter(new TestAdapter(this));
+
+        gridRoot.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
